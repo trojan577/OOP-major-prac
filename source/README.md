d10672676c3b0eb77861c7e98a6d46d453950510
trojan577 <trojan577588@gmail.com>
2017-05-09 23:48:34 +0930

    Implemented a menu system

--- source/README.md
+++ source/README.md
@@ -1,1 +1,16 @@
 Please put all of the source files in here.
+
+Have implemented a nice menu in main.cpp
+
+Compile using the makefile and run using ./a.out
+eg.
+	make
+	./a.out
+
+Then use the arrow keys to make a selection and press enter to lock in your choice.
+
+Need a player/character class with methods to move up, down, left, and right.
+Looking like it may be a top down game for simplicity :)
+
+Also I have tried to make the comments a little better for the ncurses stuff so that you may follow the code
+a little better :))
--- source/main.cpp
+++ source/main.cpp
@@ -14,51 +14,64 @@ struct sizeMax
 /* Function that returns a struct with ymax and xmax */
 sizeMax getWinSize(WINDOW *);
 
+/* Function to print the menu items and return the selection */
+int menu(WINDOW *, string[]);
+
+/* initialize the screen */
+void initialize();
+
 int main(int argc, char **argv)
-{
-	initscr(); 		//creates stdscr
-	cbreak();		//allows CTRL+C and such to force quit program
-	keypad(stdscr, TRUE);	//allows up dwn left right delete bcksp etc.
+{	
+	// Setting up ncurses parameters
+	initialize();
 
 	int yMax, xMax;
-	getmaxyx(stdscr, yMax, xMax);
+	getmaxyx(stdscr, yMax, xMax);	// Global max (for the terminal size)
 
 	int height, width;
 
-	height = 	yMax / 2; 	//integer division
-	width = 	xMax / 2;	//integer division
+	struct sizeMax terminal = getWinSize(stdscr);
 
-	/* Creating 3 windows with edges that line up */
-	WINDOW *win1 = newwin(height, width, 		0,	 	0);				// Top left
-	WINDOW *win2 = newwin(height, width, 		0, 		xMax/2);		// Top right
-	WINDOW *win3 = newwin(height, 2*(xMax/2) , 	yMax/2, 0);				// Bottom
+	height = 	yMax / 2; 	// Integer division
+	width = 	xMax / 2;	// Integer division
 
-	refresh();			//this is NEEDED! for windows to show up
+	// Creating 3 windows with edges that line up
+	WINDOW *win1 = newwin(terminal.y/2, terminal.x/2, 		0,	 	0);				// Top left window
+	WINDOW *win2 = newwin(terminal.y/2, terminal.x/2, 		0, 		xMax/2);		// Top right window
+	WINDOW *win3 = newwin(terminal.y/2, 2*(xMax/2), 		yMax/2, 0);				// Bottom window
 
+	refresh();			// Refreshing stdscr for windows to be drawn
+
+	// Getting each windows maximum x and y size (win3 just fills the rest of the screen)
+	struct sizeMax win1Size = getWinSize(win1);
+	struct sizeMax win2Size = getWinSize(win2);
+
+	// Drawing the box outline for each window
 	box(win1, 0, 0);
 	box(win2, 0, 0);
 	box(win3, 0, 0);
 
-	int win1_y_max, win1_x_max, win2_y_max, win2_x_max;
-	// getmaxyx(win1, win1_y_max, win1_x_max);
-	struct sizeMax s2 = getWinSize(win1);
-	win1_y_max = s2.y;
-	win1_x_max = s2.x;
-	getmaxyx(win2, win2_y_max, win2_x_max);
-
-	struct sizeMax s1 = getWinSize(win2);
-	win2_y_max = s1.y;
-	win2_x_max = s1.x;
-
 	// Move window print window shifts cursor before printing
-	mvwprintw(win1, win1_y_max/2 - 1, win1_x_max/2 - 9, "Centre of window 1");	
-	mvwprintw(win2, win2_y_max/2 - 1, win2_x_max/2 - 9, "Centre of window 2");	
-	mvwprintw(win3, win1_y_max/2 - 1, xMax/2 - 9, 		"Centre of window 3");	
+	mvwprintw(win1, win1Size.y/2 - 1, win1Size.x/2 - 9	, "Centre of window 1");
+	mvwprintw(win2, win2Size.y/2 - 1, win2Size.x/2 - 9	, "Centre of window 2");
+	mvwprintw(win3, win1Size.y/2 - 1, xMax/2 - 9		, "Centre of window 3");
 
+	// Refreshing all windows to show the text and the boxes
 	wrefresh(win1);
 	wrefresh(win2);
 	wrefresh(win3);
-	
+
+	werase(win3);				// Erases everything in the window
+	box(win3, 0, 0);			// Redrawing the box outline
+
+	keypad(stdscr, true);		// Grabbing input via stdscr (can use individual window)
+
+	string options[3] = {"Play", "Quit", "Help"};
+
+	int selected = menu(win3, options);
+	mvwprintw(win3, 1, 1, "You chose: %s", options[selected].c_str());
+	wrefresh(win3);
+
 	getch();		// Pause until user input
 	endwin();		// Closes stdscr and returns the terminal
 
@@ -71,4 +84,58 @@ sizeMax getWinSize(WINDOW *win)
 	getmaxyx(win, s1.y, s1.x);
 
 	return s1;
+}
+
+void initialize()
+{
+	initscr(); 				// Creates stdscr
+	cbreak();				// Allows CTRL+C and such to force quit program
+	noecho();				// Stops user input printing on the screen
+	curs_set(0);			// Stops cursor from showing
+	// resize_term(40, 120);	// Allocates terminal size (y, x)
+}
+
+int menu(WINDOW *win, string options[])
+{
+	int choice;
+	int selection = 0;
+
+	struct sizeMax winSize = getWinSize(win);
+
+	/* Menu implementation */
+	for(;;)
+	{
+		for(unsigned int i = 0; i < 3; ++i)
+		{
+			if(i == selection)
+			{
+				wattron(win, A_REVERSE);		// Highlights current selection by inverting background and text
+			}
+			mvwprintw(win, i+winSize.y/2 - 1, winSize.x/2 - 2, options[i].c_str());
+			wattroff(win, A_REVERSE);			// Removes the highlight
+			wrefresh(win);
+		}
+
+		choice = wgetch(stdscr); // Waiting for user input
+
+		switch(choice)
+		{
+			case KEY_UP:
+				if(selection > 0)
+					selection--;
+				break;
+			case KEY_DOWN:
+				if(selection < 2)
+					selection++;
+				break;
+			default:
+				break;
+		}
+
+		if(choice == 10)		// 10 == enter key
+		{
+			break;
+		}
+	}
+	return selection;
 }
