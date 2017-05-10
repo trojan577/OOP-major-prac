a86054629f9d5adf2d4c06bf3189e9e217986e37
JimmyMagic <james.gaskell@hotmail.com>
2017-05-10 14:55:36 +0930

    Add files via upload

--- map.h
+++ map.h
@@ -7,7 +7,7 @@
 
 class Map{
                 //initialises a vector of vectors with area 15 by 15
-    vecter< vector<std::string> > mapLayout(15, vector<std::string>(15));
+    vector< vector<std::string> > mapLayout(15, vector<std::string>(15));
 public:
     void displayMap();
 }
